b2786fb4fdfffdb63af8c1cd3a2f9714d4d6d66d
Stanisław Szymczyk <sszymczy@gmail.com>
2024-07-20 19:42:28 +0200

    Added prompt for DeepSeek-V2-Chat-0628.

--- run_model.py
+++ run_model.py
@@ -32,6 +32,7 @@ if system_prompt:
     PHI3_PROMPT_TEMPLATE=None
     DEEPSEEK2_PROMPT_TEMPLATE="{SYS}\n\nUser: {USER}\n\nAssistant:"
     NEMOTRON4_PROMPT_TEMPLATE="<extra_id_0>System\n{SYS}\n<extra_id_1>User\n{USER}\n<extra_id_1>Assistant\n"
+    DEEPSEEK2_0628_PROMPT_TEMPLATE="<｜begin▁of▁sentence｜>{SYS}\n\n<｜User｜>{USER}<｜Assistant｜>"
 else:
     LLAMA_PROMPT_TEMPLATE="<s>[INST] {USER}[/INST]\n"
     LLAMA3_PROMPT_TEMPLATE="<|begin_of_text|><|start_header_id|>user<|end_header_id|>\n\n{USER}<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n"
@@ -41,6 +42,7 @@ else:
     PHI3_PROMPT_TEMPLATE="<|user|>\n{USER}<|end|>\n<|assistant|>"
     DEEPSEEK2_PROMPT_TEMPLATE="User: {USER}\n\nAssistant:"
     NEMOTRON4_PROMPT_TEMPLATE="<extra_id_0>System\n\n<extra_id_1>User\n{USER}<extra_id_1>Assistant\n"
+    DEEPSEEK2_0628_PROMPT_TEMPLATE="<｜begin▁of▁sentence｜><｜User｜>{USER}<｜Assistant｜>"
 
 model_file_basename = os.path.basename(model_file)
 
@@ -57,7 +59,10 @@ elif any(model_name in model_file_basename.lower() for model_name in ["wizardlm"
 elif any(model_name in model_file_basename.lower() for model_name in ["phi-3"]):
     prompt_template = PHI3_PROMPT_TEMPLATE
 elif any(model_name in model_file_basename.lower() for model_name in ["deepseek-v2"]):
-    prompt_template = DEEPSEEK2_PROMPT_TEMPLATE
+    if "0628" in model_file_basename:
+        prompt_template = DEEPSEEK2_0628_PROMPT_TEMPLATE
+    else:
+        prompt_template = DEEPSEEK2_PROMPT_TEMPLATE
 elif any(model_name in model_file_basename.lower() for model_name in ["nemotron"]):
     prompt_template = NEMOTRON4_PROMPT_TEMPLATE
 else:
